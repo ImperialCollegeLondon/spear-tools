809038aac30ee5043e03a69501fa0410f5b1d2d6
Alastair Moore <alastair.h.moore@imperial.ac.uk>
2022-08-30 22:08:25 +0100

    Adding to examples log

--- SPEAR_analysis/ahm_examples.sh
+++ SPEAR_analysis/ahm_examples.sh
@@ -22,3 +22,20 @@ done
 
 # metrics
 python spear_evaluate.py 'spear_data/Main/Train' my_results/003_testing_baseline_20220801_171800 segments.csv my_results/004_metrics/baseline.csv
+
+
+# new examples - 30 Aug 2022
+
+# setup soft links to make paths easier
+ln -s /Volumes/SPEAR_SSD4/ahm_tests_20220830 my_results
+ln -s /Volumes/SPEAR_SSD4/v2/Volumes/SPEAR_SSD3/SPEAR_v2 spear_data
+
+# all in one go
+python spear_enhance.py 'spear_data/Main/Dev' my_results/001_testing_passthrough_20220839_113800 -m passthrough
+python spear_enhance.py 'spear_data/Main/Dev' my_results/002_testing_baseline_20220839_124200 -m baseline
+
+# in separate tabs - parallel computations
+conda activate spear-tools2
+python spear_enhance.py 'spear_data/Main/Dev' my_results/002_testing_baseline_20220839_124200 -m baseline -l D2
+python spear_enhance.py 'spear_data/Main/Dev' my_results/002_testing_baseline_20220839_124200 -m baseline -l D3
+python spear_enhance.py 'spear_data/Main/Dev' my_results/002_testing_baseline_20220839_124200 -m baseline -l D4
