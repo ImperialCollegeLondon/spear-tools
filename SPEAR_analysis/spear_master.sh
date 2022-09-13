481012adc93dee7b82ffe8c82b6b21194dda36d2
Guiraud <pierre.guiraud18@gmail.com>
2022-09-13 17:26:11 +0100

    correct metrics filename

--- SPEAR_analysis/spear_master.sh
+++ SPEAR_analysis/spear_master.sh
@@ -16,8 +16,8 @@ python spear_enhance.py "spear_data/Main/$SET" "my_results/audio_passthrough_$(d
 python spear_enhance.py "spear_data/Main/$SET" "my_results/audio_${METHOD}_$(date '+%Y%m%d')" -m $METHOD -l D$DATASET $SESSION $MINUTE
 
 # Compute metrics
-python spear_evaluate.py "spear_data/Main/$SET" "my_results/audio_passthrough_$(date '+%Y%m%d')" "segments_$SET.csv" "my_results/metrics/passthrough_D${DATASET}_S${SESSION}_M${MINUTE}.csv"  -l D$DATASET $SESSION $MINUTE -m $METRICS
-python spear_evaluate.py "spear_data/Main/$SET" "my_results/audio_${METHOD}_$(date '+%Y%m%d')" "segments_$SET.csv" "my_results/metrics/${METHOD}_D${DATASET}_S${SESSION}_M${MINUTE}.csv"  -l D$DATASET $SESSION $MINUTE -m $METRICS
+python spear_evaluate.py "spear_data/Main/$SET" "my_results/audio_passthrough_$(date '+%Y%m%d')" "segments_$SET.csv" "my_results/metrics/passthrough_${SET}_D${DATASET}_S${SESSION}_M${MINUTE}.csv"  -l D$DATASET $SESSION $MINUTE -m $METRICS
+python spear_evaluate.py "spear_data/Main/$SET" "my_results/audio_${METHOD}_$(date '+%Y%m%d')" "segments_$SET.csv" "my_results/metrics/${METHOD}_${SET}_D${DATASET}_S${SESSION}_M${MINUTE}.csv"  -l D$DATASET $SESSION $MINUTE -m $METRICS
 
 # Visualise/plot metrics
-python spear_visualise.py "my_results/metrics/${METHOD}_D${DATASET}_S${SESSION}_M${MINUTE}" "my_results/metrics/passthrough_D${DATASET}_S${SESSION}_M${MINUTE}.csv" "my_results/metrics/${METHOD}_D${DATASET}_S${SESSION}_M${MINUTE}.csv" -m $METHOD
+python spear_visualise.py "my_results/metrics/${METHOD}_${SET}_D${DATASET}_S${SESSION}_M${MINUTE}" "my_results/metrics/passthrough_${SET}_D${DATASET}_S${SESSION}_M${MINUTE}.csv" "my_results/metrics/${METHOD}_D${DATASET}_S${SESSION}_M${MINUTE}.csv" -m $METHOD
