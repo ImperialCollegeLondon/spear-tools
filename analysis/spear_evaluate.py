f037e2f902d1ef8e299c8068f543748d32adea2a
Guiraud <pierre.guiraud18@gmail.com>
2023-03-17 10:22:55 +0000

    HASPI fixed random seed

--- analysis/spear_evaluate.py
+++ analysis/spear_evaluate.py
@@ -87,6 +87,7 @@ def compute_metrics(x_proc, x_ref, fs_ref, cols):
                 elif mm=='fwSegSNR':
                     score = pysepm.fwSNRseg(x_ref[:,cc], x_proc[:,cc], fs)   
                 elif mm=='HASPI':
+                    np.random.seed(33)
                     score, _ = haspi.haspi_v2(x_ref[:,cc], fs, x_proc[:,cc], fs, [0,0,0,0,0,0])
                 else:
                     raise ValueError(f'Unknown metric: {mm}')
